bd7c3722595507e79cbc54edc8eb6706381a5e18
Jason <jasonling1995@gmail.com>
2020-12-12 18:27:45 +0800

    add to cart button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,6 +37,11 @@
                             {{ $product->user->name }}
                         </p>
                     </a>
+                    <button
+                        type="button"
+                        class="border border-indigo-500 bg-indigo-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline">
+                        Add To Cart
+                    </button>
                     <!-- <a class="no-underline text-grey-darker hover:text-red-dark" href="#">
                         <span class="hidden">Like</span>
                         <i class="fa fa-heart"></i>
